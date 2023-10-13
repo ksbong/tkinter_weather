92c65b31c8040f9178d59e4670c89776699216da
ksbong <ksbong06@gmail.com>
2023-10-13 18:09:56 +0900

    온도 단위별 변환 옵션 추가

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import json
 import base64
 from urllib.request import urlopen
 from tkinter import *
+from tkinter import ttk
 from cities import city_dict
 
 file_path = "./weather.json"
+
+tempCode = '°C'
 
 apiKey = "618414cd7a1e665025fb2cc40426aa75"
 lang = 'kr'
@@ -13,9 +16,16 @@ units = 'metric'
 
 root = Tk()
 root.title("날?씨")
-root.geometry('330x200')
+root.geometry('330x180')
+
+runOnce = False
+
 
 frame = Frame(root)
+
+listFrame = Frame(root)
+
+selectionFrame = Frame(root)
 
 frame.grid(row=0, column=1)
 
@@ -24,7 +34,15 @@ image_byt = urlopen(icon_url).read()
 image_b64 = base64.encodebytes(image_byt)
 photo = PhotoImage(data=image_b64)
 
-listbox = Listbox(root, height=10, selectmode="browse")
+listbox = Listbox(listFrame, height=10)
+listbox.pack(side="left", fill="y")
+
+scrollbar = ttk.Scrollbar(listFrame,orient="vertical",command=listbox.yview)
+
+listbox.config(yscrollcommand=scrollbar.set)
+
+scrollbar.pack(side="right", fill="y")
+
 city_dict_keys = list(city_dict.keys())
 
 keys = []
@@ -34,22 +52,46 @@ for i in range(len(city_dict_keys)):
 for i in range(len(keys)):
     listbox.insert(0, keys[i])
 
+    
+options =["섭씨 °C","화씨 ℉","캘빈 K"]
+
+selected_option = StringVar(selectionFrame)
+selected_option.set(options[0])
+
+
+dropdown = ttk.OptionMenu(selectionFrame, selected_option, options[0], *options)
+dropdown.pack(side="left", fill="y")
+
+
+
 cityLabel = Label(text="도시이름")
 
-weatherTemp = Label(frame, text="현재온도: 273K   ")
-weatherStatus = Label(frame, text="날씨: 맑?음")
-imagelab = Label(frame, image=photo)
+weatherTemp = ttk.Label(frame, text="현재온도: ???   ")
+weatherStatus = ttk.Label(frame, text="날씨: 맑?음")
+imagelab = ttk.Label(frame, image=photo)
 
-feelTemp = Label(frame, text="체감온도: 273K")
+feelTemp = ttk.Label(frame, text="체감온도: ???")
 
-max_temp = Label(frame, text="최고온도: 273K")
-min_temp = Label(frame, text="최저온도: 273K")
+max_temp = ttk.Label(frame, text="최고온도: ???")
+min_temp = ttk.Label(frame, text="최저온도: ???")
 
-humidity = Label(frame, text="습도: 100%")
+humidity = ttk.Label(frame, text="습도: ??%")
 
-wind_speed = Label(frame, text="풍속: 100m/s")
+wind_speed = ttk.Label(frame, text="풍속: ??m/s")
+
+def on_option_selected(*args):
+    selected_value = selected_option.get()
+    print("Selected Option: ", selected_value)
+    print("type: ", type(selected_value))
+    selected_option.set(selected_value)
+    # if runOnce : getWeather()
+
+selected_option.trace_add("write", on_option_selected)
 
 def getWeather():
+    global runOnce
+    if not runOnce : runOnce = True
+
     try:
         slt_index = listbox.curselection()[0]
     except:
@@ -65,9 +107,12 @@ def getWeather():
     print(result["weather"][0]["icon"], type(result["weather"][0]["icon"]))
 
     iconUrl = "http://openweathermap.org/img/w/"+ result["weather"][0]["icon"] +".png"
+
     byt = urlopen(iconUrl).read()
     b64 = base64.encodebytes(byt)
+
     photo = PhotoImage(data=b64)
+
     imagelab.configure(image=photo)
     imagelab.image = photo
 
@@ -81,16 +126,49 @@ def getWeather():
     weatherStatus["text"] = f"날씨: {wStatus}"
 
     wTemp = result["main"]["temp"]
-    weatherTemp["text"] =  f"현재온도: {round(wTemp + 273.15)}K   "
+    selected = selected_option.get()
+
 
     wfTemp = result["main"]["feels_like"]
-    feelTemp["text"] = f"체감온도: {round(wfTemp + 273.15)}K"
-
     temp_max = result["main"]["temp_max"]
-    max_temp["text"] = f"최고온도: {round(temp_max + 273.15)}K   "
-
     temp_min = result["main"]["temp_min"]
-    min_temp["text"] = f"최저온도: {round(temp_min + 273.15)}K"
+
+    ## options =["섭씨 °C","화씨 ℉","캘빈 K"]
+
+    if selected == options[0]: # 섭씨일 경우
+
+        tempCode = '°C'
+        current_Temp = round(wTemp)
+        perceived_Temp = round(wfTemp)
+        max_Temp = round(temp_max)
+        min_Temp = round(temp_min)
+
+    elif selected == options[1]: # 화씨일 경우
+
+        tempCode = '℉'
+        current_Temp = round(wTemp*(9/5)+32)
+        perceived_Temp = round(wfTemp*(9/5)+32)
+        max_Temp = round(temp_max*(9/5)+32)
+        min_Temp = round(temp_min*(9/5)+32)
+        
+    elif selected == options[2]: # 캘빈일 경우
+
+        tempCode = 'K'
+        current_Temp = round(wTemp + 273.15)
+        perceived_Temp = round(wfTemp + 273.15)
+        max_Temp = round(temp_max + 273.15)
+        min_Temp = round(temp_min + 273.15)
+
+    weatherTemp["text"] =  f"현재온도: {current_Temp}{tempCode}   "
+
+    
+    feelTemp["text"] = f"체감온도: {perceived_Temp}{tempCode}"
+
+    
+    max_temp["text"] = f"최고온도: {max_Temp}{tempCode}   "
+
+    
+    min_temp["text"] = f"최저온도: {min_Temp}{tempCode}"
 
     value_humidity =  result["main"]["humidity"]
     humidity["text"] = f"습도: {round(value_humidity)}%   "
@@ -99,7 +177,9 @@ def getWeather():
     wind_speed["text"] = f"풍속: {round(speed_wind)}m/s"
 
 
-getWeather_btn = Button(text="날?씨" ,command=getWeather)
+getWeather_btn = ttk.Button(selectionFrame, text="날?씨" ,command=getWeather)
+
+getWeather_btn.pack(side="right", fill="y")
 
 
 weatherStatus.grid(row=0, column=0)
@@ -114,8 +194,8 @@ min_temp.grid(row=2, column=1)
 humidity.grid(row=3, column=0)
 wind_speed.grid(row=3, column=1)
 
-getWeather_btn.grid(row=2, column=1)
+selectionFrame.grid(row=2, column=1)
 
-listbox.grid(row=0,column=0, rowspan=3)
+listFrame.grid(row=0,column=0, rowspan=3)
 
 root.mainloop()
